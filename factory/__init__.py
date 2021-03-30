d118975f8ffbc4771d9acaa47433f505cacfe8bf
alaa-nabawy <32427497+alaa-nabawy@users.noreply.github.com>
2021-03-30 15:04:41 +0200

    Update __init__.py

--- factory/__init__.py
+++ factory/__init__.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SECRET_KEY'] = '3ca90a04c1a9ff7715ace458401960f674cd9ed84ef93004242e3bfc6e44735d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cvpimsytamrpwe:3ca90a04c1a9ff7715ace458401960f674cd9ed84ef93004242e3bfc6e44735d@ec2-3-231-194-96.compute-1.amazonaws.com:5432/d2f00l61k9nd4h'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
